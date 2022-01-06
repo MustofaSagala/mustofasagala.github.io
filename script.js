79e3b472d173ee63fe1efc3faa30e6912d707b74
MustofaSagala <53543369+MustofaSagala@users.noreply.github.com>
2022-01-06 13:05:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
-$(function() {
-  $('.toggle-btn').click(function() {
-    $('.filter-btn').toggleClass('open');
+// function([string1, string2],target id,[color1,color2])    
+ consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
 
-  });
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
 
-  $('.filter-btn a').click(function() {
-    $('.filter-btn').removeClass('open');
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
 
-  });
+    } else {
+      con.className = 'console-underscore'
 
-});
-
-$('#all').click(function() {
-
-  $('ul.tasks li').slideDown(300);
-
-});
-
-$('#one').click(function() {
-  $('.tasks li:not(.one)').slideUp(300, function() {
-    $('.one').slideDown(300);
-
-  });
-});
-
-$('#two').click(function() {
-  $('.tasks li:not(.two)').slideUp(300, function() {
-    $('.two').slideDown(300);
-
-  });
-});
-$('#three').click(function() {
-  $('.tasks li:not(.three)').slideUp(300, function() {
-    $('.three').slideDown(300);
-
-  });
-});
+      visible = true;
+    }
+  }, 400)
+}
